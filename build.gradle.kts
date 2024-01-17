20c491dea45f1723c68324af4d61207d543676e1
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-01-17 17:46:21 +0000

    check style added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     java
+    checkstyle
+}
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    val archive = configurations.checkstyle.get().resolve().filter {
+        it.name.startsWith("checkstyle")
+    }
+    config = resources.text.fromArchiveEntry(archive, "google_checks.xml")
 }
