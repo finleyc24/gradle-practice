04002886094fb57ec4e67799b912e7317e767efa
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-01-19 17:50:13 +0000

    created names and jar test config

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,16 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+tasks.named<ProcessResources>("processResources") {
+    eachFile{
+        println("Copying: $sourceName to $destinationDir")
+    }
+}
+
+tasks.jar{
+    val archiveName by archiveFileName
+        eachFile {
+            println("Jar file: $sourceName will be included in $archiveName")
+            }
+        }
